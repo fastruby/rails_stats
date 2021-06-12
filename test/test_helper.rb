27645936cfcb5087739bd858b48c570ed35034c7
Ernesto Tagwerker <ernesto+github@ombulabs.com>
2021-06-11 21:31:17 -0400

    Require codecov and simplecov before loading test/lib code
    This will be needed to report data to CodeCov and to the console

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,5 +1,23 @@
 # frozen_string_literal: true
 
+if ENV["COVERAGE"] == "true"
+  require "simplecov"
+  require "simplecov-console"
+  require "codecov"
+
+  SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
+    SimpleCov::Formatter::HTMLFormatter,
+    SimpleCov::Formatter::Console,
+    SimpleCov::Formatter::Codecov,
+  ]
+
+  SimpleCov.start do
+    track_files "lib/**/*.rb"
+  end
+
+  puts "Using SimpleCov v#{SimpleCov::VERSION}"
+end
+
 require "byebug"
 
 require "minitest/autorun"
